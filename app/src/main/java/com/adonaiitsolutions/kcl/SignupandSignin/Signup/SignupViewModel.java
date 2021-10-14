5d7d15c33cb857fffe19f14ef6308d8dc3cc7f4a
Bhara <Bharathkumar>
2021-10-14 21:16:10 +0530

    Volley update

--- app/src/main/java/com/adonaiitsolutions/kcl/SignupandSignin/Signup/SignupViewModel.java
+++ app/src/main/java/com/adonaiitsolutions/kcl/SignupandSignin/Signup/SignupViewModel.java
@@ -1,39 +1,31 @@
 package com.adonaiitsolutions.kcl.SignupandSignin.Signup;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
-import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.ViewModel;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 
 import java.util.HashMap;
 import java.util.Map;
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import retrofit2.Call;
-import retrofit2.Callback;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
+
+
 
 public class SignupViewModel extends AndroidViewModel {
 String Response;
+@SuppressLint("StaticFieldLeak")
 Context context;
 
     public SignupViewModel(@NonNull Application application) {
@@ -52,20 +44,11 @@ Context context;
             String url="http://aiccollege.com/PHP/signup.php";
 
         StringRequest stringRequest=new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("TAG", "onResponse: "+ response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("TAG", "onErrorResponse: "+error.toString());
-            }
-        }){
+                response -> Log.d("TAG", "onResponse: "+ response),
+                error -> Log.d("TAG", "onErrorResponse: "+error.toString())){
             @Override
             protected Map<String, String> getParams()  {
-                Map<String,String>parms=new HashMap<String, String>();
+                Map<String,String>parms= new HashMap<>();
                 parms.put("name",name);
                 parms.put("email",email);
                 parms.put("fname",father_name);
