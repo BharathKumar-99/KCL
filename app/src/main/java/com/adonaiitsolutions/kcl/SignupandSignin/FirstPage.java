96e74104569cdbdd3cc85b9a3519ea5bd937a889
AdonaiDevRig <Bharathkumar6362@gmail.com>
2021-10-15 10:10:34 +0530

    first page added

---
+++ app/src/main/java/com/adonaiitsolutions/kcl/SignupandSignin/FirstPage.java
@@ -0,0 +1,16 @@
+package com.adonaiitsolutions.kcl.SignupandSignin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.adonaiitsolutions.kcl.R;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
---
+++ app/src/main/java/com/adonaiitsolutions/kcl/SignupandSignin/Signin/Signin.java
@@ -0,0 +1,16 @@
+package com.adonaiitsolutions.kcl.SignupandSignin.Signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.adonaiitsolutions.kcl.R;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
